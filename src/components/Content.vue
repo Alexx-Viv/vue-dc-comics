0c13ae87b4a9609fe6ca5310bd0fe60019709ed4
Alexx-Viv <81645879+Alexx-Viv@users.noreply.github.com>
2021-09-08 12:50:12 +0200

    Add comicx

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,29 +1,60 @@
 <template>
   <section>
     <div class="container">
-      <h1>Content Goes Here</h1>
+      <div class="card" v-for="comic in DcComics" :key="comic">
+        <figure>
+          <img :src="comic.thumb" :alt="comic.series" />
+          <figcaption>{{ comic.series }}</figcaption>
+        </figure>
+      </div>
     </div>
+    <div class="button">Load More</div>
   </section>
 </template>
 
 <script>
+import DcComics from '../data/dc-comics.js';
 export default {
   name: 'Content',
+  data() {
+    return {
+      DcComics,
+    };
+  },
 };
 </script>
 
 <style scoped>
 section {
   width: 100%;
-  height: 100px;
+  height: 100%;
   background-color: black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 5vh;
 }
+
 .container {
   height: 100%;
+  max-width: 1200px;
   display: flex;
   align-items: center;
+  flex-wrap: wrap;
 }
-h1 {
+.card {
+  flex-basis: calc(100% / 6);
+  margin: 5vh;
+}
+figcaption {
+  margin-top: 2vh;
   color: white;
+}
+
+.button {
+  background-color: #0082f9;
+  color: white;
+  padding: 10px 25px;
+  cursor: pointer;
 }
 </style>
